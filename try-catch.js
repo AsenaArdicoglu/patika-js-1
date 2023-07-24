11514b1f1edd3e33bbc82c40f22f727e009d1694
Asena Ardıçoğlu <asenaardicc@gmail.com>
2023-07-24 18:34:15 +0300

    try catch

---
+++ try-catch.js
@@ -0,0 +1,13 @@
+// hata yonetimi
+let products;
+
+try {
+    products.forEach(item => console.log(item));       
+    } catch(error) {
+        console.log("error: " , error);
+    }
+
+    console.log("hata yometimi duzgun calisiyor");
+
+    let info = "kodluyoruz"
+    console.log(info);
