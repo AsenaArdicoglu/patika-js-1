0789a756a16892f00210a2c1b52867a482f1d4fb
Asena Ardıçoğlu <asenaardicc@gmail.com>
2023-07-24 15:58:47 +0300

    js 2

---
+++ template2.js
@@ -0,0 +1,6 @@
+let lastChild = document.querySelector("ul#list>li:last-child")
+lastChild.innerHTML ="son oge degisti"
+
+let firstChild = document.querySelector("ul#list>li:first-child")
+firstChildtChild.innerHTML ="ilk  oge degisti"
+
