53a9649d5fce45db4ef7857314e65d7102b6dcb0
Asena Ardıçoğlu <asenaardicc@gmail.com>
2023-07-24 15:58:55 +0300

    js 3

---
+++ template3.js
@@ -0,0 +1,14 @@
+// let greeting = document.querySelector("#greeting")
+
+// greeting.classList.add("text-primary")
+// greeting.classList.add("title")
+// greeting.classList.add("new-info", "second-class", "third-class")
+
+
+// greeting.classList.remove("new-info",  "second-class", )
+
+// console.log(greeting.classList)
+
+let div = document.querySelector("#greetings");
+div.classList.add("info");
+
